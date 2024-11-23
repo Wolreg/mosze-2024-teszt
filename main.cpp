279cc929efa34cf9b4941bb5ec936ec4fbc4c54c
Gergő Farkas <farkas8891@gmail.com>
2024-11-23 23:14:41 +0100

    Hibák javítása a main.cpp fájlban

--- main.cpp
+++ main.cpp
@@ -4,39 +4,31 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // Hibásan van megadva az "NELEMENTS", helyesen "N_ELEMENTS"
-    int *b = new int[NELEMENTS];
-
-    // Kiiratással van gond, mert ' jel van használva " helyett, pontos vessző (;) hiánya
-    std::cout << '1-100 ertekek duplazasa'
+    int *b = new int[N_ELEMENTS];
     
-    // Kell egy feltétel és egy kezdőérték
-    for (int i = 0;)
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
     
-    // Ciklus vége nincs meghatározva
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-       
-       // hiányzik a pontosvessző (;)
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     
-    // "atlag" változót inicializálni kell először
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    
-    //  i < N_ELEMENTS feltétel után hibás a vessző, hiszen pontosvessző (;) kell oda
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // Pontosvessző (;) hiánya
-        atlag += b[i]
+        atlag += b[i];
     }
-
-    // "atlag" változó nincs inicializálva
+    
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    
+    delete[] b;
+
     return 0;
 }
